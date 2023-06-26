b8d5bfa4f0085dba4c0e9ac8acd143d40e959637
Vitchenzo2812 <gvitchenzo@gmail.com>
2023-06-26 14:32:09 -0300

    feat: default configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,9 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
-
-const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
-}
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
